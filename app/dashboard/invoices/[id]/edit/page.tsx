079a93bf0cb421fb22bf96fd3b976120f574cc28
Kevin-Olazo <kevind.olazo@gmail.com>
2025-10-14 01:53:27 -0500

    [update]: added updateInvoice action, pass the id using JS bind in edit-form

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -22,6 +22,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           },
         ]}
       />
+
       <Form invoice={invoice} customers={customers} />
     </main>
   );
