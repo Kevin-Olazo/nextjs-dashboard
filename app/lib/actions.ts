17cfb5316e5664770fd726684c69b30e285dfafb
Kevin-Olazo <kevind.olazo@gmail.com>
2025-10-14 12:07:51 -0500

    [update]: added validation to createInvoice Action Server

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -33,24 +33,39 @@ export type State = {
 };
 
 export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form fields using Zod
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  // If form validation fails, return error early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice",
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
+  // Insert data into the database
   try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
   } catch (error) {
-    console.error("Error creating invoice:", error);
+    // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice",
     };
   }
+
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
